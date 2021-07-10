e9432f8198138f04dac97389d9b1b28acec76c98
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2021-07-10 17:59:02 +0300

    global variable

--- app.py
+++ app.py
@@ -7,26 +7,27 @@ import os
 import json
 app=Flask(__name__,template_folder='templates')
 app.secret_key=os.urandom(24)
+header=["Sultanbeyli","Esenler","Uskudar","Zeytinburnu","Kcekmece","Ortakoy"]
+file = open('mobese.json',) 
+data = json.load(file)
+file.close()
 @app.route('/')
 def index():
     return render_template('index.html')
 @app.route('/city')
 def city():
-    header=["Sultanbeyli","Esenler","Uskudar","Zeytinburnu","Kcekmece","Ortakoy"]
+    global header
     cities=[]
-    file = open('mobese.json',) 
-    data = json.load(file)
+    global data
     for head in header:
         rows=[data[head]["id"],data[head]["name"],data[head]["link"],data[head]["img"]]
         cities.append(rows)
-    file.close()
     return render_template('city.html',cities=cities,color="color")
 @app.route('/detectCity' ,defaults={'city':'default'})
 @app.route('/detectCity/<city>')
 def detectCity(city):
-    header=["Sultanbeyli","Esenler","Uskudar","Zeytinburnu","Kcekmece","Ortakoy"]
-    file = open('mobese.json',) 
-    data = json.load(file)
+    global header
+    global data
     cities=[]
     link=""
     if city in header:
@@ -40,13 +41,11 @@ def detectCity(city):
     else:
         return redirect(url_for('city'))
     return render_template("detectCity.html",cities=cities,cityname=cityname,color="color",imglink=cityimg,link=link)
-    file.close()
 @app.route('/streamVideo' ,defaults={'video':'default'})
 @app.route('/streamVideo/<video>')
 def streamVideo(video):
-    file = open('mobese.json',) 
-    data = json.load(file)
-    header=["Sultanbeyli","Esenler","Uskudar","Zeytinburnu","Kcekmece","Ortakoy"]
+    global header
+    global data
     if video in header:
         link=data[video]["link"]
         socialDistance=SocialDistance(link)
