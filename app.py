a7f761bab76c98e9aeb4d01de936122401afcd34
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2021-07-10 21:13:03 +0300

    update

--- app.py
+++ app.py
@@ -62,5 +62,7 @@ def streamVideo(video):
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect(url_for('index'))
-
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=PORT, debug=False)
 
