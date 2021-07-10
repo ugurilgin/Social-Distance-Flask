517fdb8c4f0f9dea9636947aac2293e4c6412ac1
ugurilgin <43095839+ugurilgin@users.noreply.github.com>
2021-07-10 18:00:15 +0300

    format style

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ from flask import request ,jsonify,Response
 from flask import render_template,redirect,url_for
 import os
 import json
+
 app=Flask(__name__,template_folder='templates')
 app.secret_key=os.urandom(24)
+
 header=["Sultanbeyli","Esenler","Uskudar","Zeytinburnu","Kcekmece","Ortakoy"]
 file = open('mobese.json',) 
 data = json.load(file)
 file.close()
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 @app.route('/city')
 def city():
     global header
@@ -23,6 +27,7 @@ def city():
         rows=[data[head]["id"],data[head]["name"],data[head]["link"],data[head]["img"]]
         cities.append(rows)
     return render_template('city.html',cities=cities,color="color")
+
 @app.route('/detectCity' ,defaults={'city':'default'})
 @app.route('/detectCity/<city>')
 def detectCity(city):
@@ -41,6 +46,7 @@ def detectCity(city):
     else:
         return redirect(url_for('city'))
     return render_template("detectCity.html",cities=cities,cityname=cityname,color="color",imglink=cityimg,link=link)
+
 @app.route('/streamVideo' ,defaults={'video':'default'})
 @app.route('/streamVideo/<video>')
 def streamVideo(video):
@@ -52,8 +58,10 @@ def streamVideo(video):
     else:
         return redirect(url_for('city'))
     return Response(socialDistance.Detect(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @app.errorhandler(404)
 def page_not_found(e):
     return redirect(url_for('index'))
+
 if __name__ == "__main__":
     app.run(debug=False)
